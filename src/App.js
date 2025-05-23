8976d6a0be4d532553fcbef1bcce2a0ed983a823
denis <denis.baldin04@mail.ru>
2025-05-23 19:53:44 +0300

    домашнее задание

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /*1 задача*/
+
+    const array = [22, 0, -45, 1001, 8, -6, -0.1, 101, 2.5, -55555];
+    const positive = array.filter((number) => number > 0);
+    const listItems = positive.map(item => (<li>{item}</li>))
+
+    /*2 задача*/
+
+
+    const messages = [
+        {message: 'hello', error: true},
+        {message: 'javascript', error: false},
+        {message: 'lincode', error: true},
+        {message: 'react', error: true},
+        {message: 'angular', error: false},
+        {message: 'es6', error: false},
+    ];
+    const ItemsFiltered = messages.filter(item => item.error == false);
+    console.log(ItemsFiltered);
+
+    /*3 задача*/
+
+
+    const words = ['lincode', 'js', 'react', 'css', 'angular', 'bootstrap', 'html'];
+
+    const WordsItem = words.map(item => {
+        if (
+            item.length <= 5
+        ) {
+            return '*'
+        }
+        return item
+    })
+
+
+    console.log(WordsItem);
+
+
+    return (
+        <div className="App">
+            <ul>
+                {listItems}
+
+            </ul>
+        </div>
+    );
 }
 
 export default App;
